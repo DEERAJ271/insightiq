309b3e4b1ee41a28fc4ba11a247d8c05c4a69291
DEERAJ271 <the-email-associated-with-your-DEERAJ271-github-account@example.com>
2026-07-13 14:21:02 +0530

    Add Airflow via Docker Compose: confirmed working with hello_world DAG

---
+++ airflow/dags/hello_world_dag.py
@@ -0,0 +1,22 @@
+from airflow import DAG
+from airflow.operators.bash import BashOperator
+from datetime import datetime
+
+with DAG(
+    dag_id="hello_world",
+    start_date=datetime(2026, 1, 1),
+    schedule=None,
+    catchup=False,
+    tags=["test"],
+) as dag:
+    task1 = BashOperator(
+        task_id="say_hello",
+        bash_command="echo 'Hello from Airflow'",
+    )
+
+    task2 = BashOperator(
+        task_id="say_goodbye",
+        bash_command="echo 'InsightIQ Airflow setup confirmed working'",
+    )
+
+    task1 >> task2
